480607315cd8abd07b10e09c256341e40a55a8f0
JLCarveth <jlcarveth@gmail.com>
2022-06-15 15:17:22 -0400

    Fix version number

--- log.js
+++ log.js
@@ -3,7 +3,7 @@ import os from 'os';
 /**
  * @author John L. Carveth
  * @name log.js
- * @version 0.1.0
+ * @version 0.5.2
  * @description A dead-simple logging utility that formats log messages
  * and outputs them to a JSON format.
  */
