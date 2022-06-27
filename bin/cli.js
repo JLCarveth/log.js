7023c005b2d1e4b3da4738addb2c253c21f6cf37
JLCarveth <jlcarveth@gmail.com>
2022-06-27 08:39:47 -0400

    cli argument-parsing

--- bin/cli.js
+++ bin/cli.js
@@ -12,7 +12,30 @@ let man = "\t\tlog.js v1.1.0\n" +
         BOLD + "DESCRIPTION" + RESET + "\n" +
         "\tAppend a log message to standard output.";
 
-let argRegex = /-[A-Za-z]/
+// Possible flags that can be passed to logger and if a value is expected
+// As of now, only one flag is supported, but adding additional flags this way
+// is trivial
+let flags = [
+    {
+        "flag" : "-l",
+        "name" : "--level",
+        "tag" : "level",
+        "expectValue" : true    // Whether the CLI expects a value w/ flag
+                                // "info", "warning","error","fatal", or any
+                                // other log level you chose
+        // Usage:
+        //  -l info
+        // --level warning
+    }
+]
+
+let pargs = {}      // Contains all arguments once parsed
+let toSkip = [0,1]; // Tracks IDs of args to ignore
+let data = {};
+let message = '';
+
+let argRegex = /-+[A-Za-z]/ // Regex used to match command-line arguments ex. "-l"
+let kvRegex = /[A-Za-z0-9]*=[A-Za-z0-9]*/ // Matches key=value pairs
 
 /**
  * First argument is always node binary,
@@ -30,5 +53,44 @@ if (process.argv.length === 2) {
     // Parse arguments, look for level=[a-zA-Z]*
     // Otherwise it's all data arguments x=42
     // Iterate through args looking for ones that match
-    
+    process.argv.forEach((arg, index) => {
+        if (!toSkip.includes(index)) {
+            if (argRegex.test(arg)) {
+                // If argument is identified as a "flag" ie "-l" or "--something"
+                parseArgument(arg, index);
+            } else if (kvRegex.test(arg)) {
+                data[arg.split('=')[0]] = arg.split('=')[1];
+            } else {
+                // Assume parameter is message
+                message = arg;
+            }
+        }
+    });
+    Log.log(message, pargs.level, data);
+}
+
+/**
+ * Parses a single argument to see if it's a valid flag.
+ * @param {*} arg Argument to be parsed
+ * @param {*} index position of the argument with process.argv
+ */
+function parseArgument(arg, index) {
+    flags.forEach(flag => {
+        if (arg === flag.name || arg === flag.flag) {
+            if (flag.expectValue) {
+                // flag expects a value, ensure there are enough arguments
+                if (index < process.argv.length - 1) {
+                    pargs[flag.tag] = process.argv[index + 1];
+                    toSkip.push(index + 1);
+                } else {
+                    console.log("Not enough parameters.")
+                    process.exit(9); //Invalid Argument exit code s
+                }
+            } else {
+                // Flag doesn't expect value, so just store the fact that
+                // it was passed
+                pargs[flag.tag] = true;
+            }
+        }
+    });
 }
